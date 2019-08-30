3e86c78f2cad95884634a83c2763e91d17328f7f
Sid <sidhumeher@yahoo.co.in>
2019-08-30 15:13:13 -0700

    Review complete

--- src/Coding/GenerateParanteses.py
+++ src/Coding/GenerateParanteses.py
@@ -4,27 +4,27 @@ Created on Aug 27, 2019
 @author: siddardha.teegela
 '''
 
+'''
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+'''
+
 def generateParanteses(n):
-    '''
-    parens = []
-    left = right = n
-    
-    def generate(p,left,right):
-        if left:
-            generate(p+'(', left-1, right)
-        if right > left:
-            generate(p+')', left, right-1)
-        if not right:
-            parens.append(p)
-        return parens
-    
-    generate('', left, right)
-    '''
     
     ret = set()
     memo = set()
     def f(p):
         #print(p)
+        print(memo)
         if p in memo:
             return
         memo.add(p)
